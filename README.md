1a2619b98cfecfad776cde147c1a72f852e163d5
Prrorr2 <rubenantolinjr@gmail.com>
2023-11-01 14:24:57 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#This is a project to convert Image of any size to ASCII ART
+##To get started
+git clone https://github.com/Prorr2/Image2Ascii
+cd Image2Ascii
+pip install -r requirements.txt
+python3 main.py
--- main.py
+++ main.py
@@ -7,26 +7,26 @@ from tqdm import tqdm
 from tkinter import filedialog
 import webbrowser
 image = cv.imread(filedialog.askopenfilename(filetypes=[("Image", [".png", ".jpeg", ".jpg"])]))
-image_rgb=cv.cvtColor(image, cv.COLOR_BGR2RGB) 
-ascii_image = [['X' for _ in range(len(image_rgb))] for _ in range(len(image_rgb))]
+imageRgb=cv.cvtColor(image, cv.COLOR_BGR2RGB) 
+asciiMatrix = [['X' for _ in range(len(imageRgb))] for _ in range(len(imageRgb))]
 print("Analazing Photo")
-for x in tqdm(range(len(image_rgb))):
-    for y in range(len(image_rgb)):
-         sum = image_rgb[x,y][0] + image_rgb[x,y][1] + image_rgb[x,y][2]
-         if sum < 160:
-              image_rgb[x,y] = [0,0,0]
-              ascii_image[x][y] = random.choice(string.ascii_lowercase)
+for x in tqdm(range(len(imageRgb))):
+    for y in range(len(imageRgb)):
+         totalColor = imageRgb[x,y][0] + imageRgb[x,y][1] + imageRgb[x,y][2]
+         if totalColor < 160:
+              imageRgb[x,y] = [0,0,0]
+              asciiMatrix[x][y] = random.choice(string.ascii_lowercase)
          else:
-              image_rgb[x,y] = [255,255,255]
-              ascii_image[x][y] = " "
-c = canvas.Canvas("resultado.pdf")
+              imageRgb[x,y] = [255,255,255]
+              asciiMatrix[x][y] = " "
+c = canvas.Canvas("result.pdf")
 c.setFont("Helvetica", 1)
 print("Creating Result")
-for x in tqdm(range(len(ascii_image))):
-     for y in range(len(ascii_image)):
+for x in tqdm(range(len(asciiMatrix))):
+     for y in range(len(asciiMatrix)):
           try:
-           c.drawString(x,y,ascii_image[len(ascii_image) - y][len(ascii_image) - x])
+           c.drawString(x,y,asciiMatrix[len(asciiMatrix) - y][len(asciiMatrix) - x])
           except Exception:
               c.drawString(x,y," ")
 c.save()
-webbrowser.open_new("resultado.pdf")
+webbrowser.open_new("result.pdf")
