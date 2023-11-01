32be5061f00032d34dc179c2c011f7ec02107690
Prrorr2 <rubenantolinjr@gmail.com>
2023-11-01 15:44:50 +0100

    resize

--- main.py
+++ main.py
@@ -6,27 +6,55 @@ from reportlab.pdfgen import canvas
 from tqdm import tqdm
 from tkinter import filedialog
 import webbrowser
+from pdf2image import convert_from_path
+option = input("Do you want yo change image size? (y/n)")
+aceptableOptionList = ["yes", "y"]
+px = 0
+if option in aceptableOptionList:
+    px = int(input("px in width and heigh: "))
 image = cv.imread(filedialog.askopenfilename(filetypes=[("Image", [".png", ".jpeg", ".jpg"])]))
-imageRgb=cv.cvtColor(image, cv.COLOR_BGR2RGB) 
-asciiMatrix = [['X' for _ in range(len(imageRgb))] for _ in range(len(imageRgb))]
+if option in aceptableOptionList:
+     image_rgb=cv.cvtColor(cv.resize(image, (px, px)), cv.COLOR_BGR2RGB) 
+ascii_image = [['X' for _ in range(len(image_rgb))] for _ in range(len(image_rgb))]
+print("-----------------------------------------------------------------------")
 print("Analazing Photo")
-for x in tqdm(range(len(imageRgb))):
-    for y in range(len(imageRgb)):
-         totalColor = imageRgb[x,y][0] + imageRgb[x,y][1] + imageRgb[x,y][2]
-         if totalColor < 160:
-              imageRgb[x,y] = [0,0,0]
-              asciiMatrix[x][y] = random.choice(string.ascii_lowercase)
+print("-----------------------------------------------------------------------")
+for x in tqdm(range(len(image_rgb))):
+    for y in range(len(image_rgb)):
+         sum = image_rgb[x,y][0] + image_rgb[x,y][1] + image_rgb[x,y][2]
+         if sum < 160:
+              image_rgb[x,y] = [0,0,0]
+              ascii_image[x][y] = random.choice(string.ascii_lowercase)
          else:
-              imageRgb[x,y] = [255,255,255]
-              asciiMatrix[x][y] = " "
+              image_rgb[x,y] = [255,255,255]
+              ascii_image[x][y] = " "
 c = canvas.Canvas("result.pdf")
 c.setFont("Helvetica", 1)
-print("Creating Result")
-for x in tqdm(range(len(asciiMatrix))):
-     for y in range(len(asciiMatrix)):
+print("-----------------------------------------------------------------------")
+print("Creating Image")
+print("-----------------------------------------------------------------------")
+for x in tqdm(range(len(ascii_image))):
+     for y in range(len(ascii_image)):
           try:
-           c.drawString(x,y,asciiMatrix[len(asciiMatrix) - y][len(asciiMatrix) - x])
+           c.drawString(x,y,ascii_image[len(ascii_image) - y][len(ascii_image) - x])
           except Exception:
               c.drawString(x,y," ")
+              
 c.save()
-webbrowser.open_new("result.pdf")
+page = convert_from_path('result.pdf')
+page[0].save('result.jpg', 'JPEG')
+
+file = open("result.txt", "w")
+print("-----------------------------------------------------------------------")
+print("Creating Text")
+print("-----------------------------------------------------------------------")
+for x in tqdm(range(len(ascii_image))):
+     for y in range(len(ascii_image)):
+          try:
+           file.write(ascii_image[x][y])
+          except Exception:
+              file.write(" ")
+     file.write("\n")
+file.close()
+webbrowser.open_new_tab("result.jpg")
+webbrowser.open_new_tab("result.txt")
